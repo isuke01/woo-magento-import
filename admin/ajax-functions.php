4273b0f4ea61dde724ad934d65d05f29630cb880
Łukasz Isu <isu@isuke.pl>
2018-10-16 18:39:03 +0200

    fix some issues, fix error handling, add force refresh products etc.

--- admin/ajax-functions.php
+++ admin/ajax-functions.php
@@ -26,13 +26,13 @@ function import_csv_products(){
     }else{
         $items = count($csv); // count imtes in CSV
     }
-    $step = 10; // import items per step
+    $step = 20; // import items per step
 
     $outut_html = '';
     // start table
-    if ($current_item == 1) {
+    if ($current_item <= 1) {
     	$outut_html .= '<tr>';
-    	$exp_keys = [ 'ID', 'Obrazek', 'Produkt', 'SKU', 'Cena', 'Kategorie'];
+    	$exp_keys = [ 'ID', 'Obrazek', 'Produkt', 'SKU', 'Cena', 'Kategorie','Type'];
     	foreach ($exp_keys as $value) {
     		$outut_html .= '<th>'.$value.'</th>';
     	}
@@ -40,31 +40,42 @@ function import_csv_products(){
     }
 
     // //first is CSV KEY
+    $excluded = 0;
+    $product_ibnsert = [];
     for ($i=1; $i <= $step ; $i++) {
     	if( $current_item > $items || !isset( $csv[$current_item] ) ) break;
         $product_data = isu_ma2_product_woo_data( $csv[$current_item], $media_dir );
 
         $test = ob_get_clean();
         $current_item++;
-        if( $product_data['store_view_code'] !== '' ) continue;
-    	if( !$test_mode ){ input_product_exported( $product_data ); }
-        
+        // We want without any lang
+        if( $product_data['store_view_code'] !== '' ) {
+            $excluded = $excluded +1;
+            continue;
+        }
+		$insert_type['type'] = 'debug';
+    	if( !$test_mode ){ 
+            $product_ibnsert[] = $insert_type = input_product_exported( $product_data );
+        }
 
         //debug LOG
     	$outut_html .= '<tr><td>'.$current_item.'</td>';
     	$outut_html .= '<td><img style="height: 80px; width: auto;" src="'.$product_data['thumbnail_image'].'"/></td>';
-		    $outut_html .= '<td>'.$product_data['name'].'</td>';
-		    $outut_html .= '<td>'.$product_data['sku'].'</td>';
+		$outut_html .= '<td>'.$product_data['name'].'</td>';
+		$outut_html .= '<td>'.$product_data['sku'].'</td>';
     	$outut_html .= '<td>'.$product_data['price'].'</td>';
-    	$outut_html .= '<td>'.$product_data['categories'].'</td>';
+        $outut_html .= '<td>'.$product_data['categories'].'</td>';
+        $outut_html .= '<td><code>'.$insert_type['type'].'</code></td>';
         $outut_html .= '</tr>';
     }
     
 	wp_send_json( [
         'type'	=>  'log',
         'items' => $items,
+        'excluded' => $excluded,
         'current_item' => $current_item,
         'html'  => $outut_html,
+        'actions'  =>  $product_ibnsert,
 	    'percent'  =>  round( ( (($current_item-1)/$items)*100), 2), //we doing -1 because 1st is csv index
 	    //'max'	   =>  2,
     ]);
@@ -102,139 +113,179 @@ function isu_ma2_product_woo_data( $csv_data, $dir ){
 	$wanted_data = [];
 	foreach ($keys as $key => $key_id) {
 		if( in_array($key_id, $images_keys) ){
-			$wanted_data[$key] = $url_conten.$csv_data[$key_id];
+			$wanted_data[$key] = isset($csv_data[$key_id])? $url_conten.$csv_data[$key_id] : null;
 		}else{
-			$wanted_data[$key] = $csv_data[$key_id];	
+            if(isset($csv_data[$key_id])){}
+			$wanted_data[$key] = isset($csv_data[$key_id])? $csv_data[$key_id] : null;	
 		}
 	}
 	return $wanted_data;
 }
 
 
-
+/*
+* https://www.skyverge.com/blog/find-product-sku-woocommerce/
+*/
+function get_product_by_sku_isu( $sku ) {
+	global $wpdb;
+	$product_id = $wpdb->get_var( $wpdb->prepare( "SELECT post_id FROM $wpdb->postmeta WHERE meta_key='_sku' AND meta_value='%s' LIMIT 1", $sku ) );
+	return $product_id;
+}
 
 function input_product_exported( $wanted_data ){
 	$user = wp_get_current_user();
-	$user_id = $user->ID;
-	$post_id = wp_insert_post( array(
-       'post_author'  =>1,
-       'post_title'   => $wanted_data['name'],
-       'post_content' => $wanted_data['description'],
-       'post_status'  => 'publish',
-       'post_type'    => 'product',
-   ) );
-   //create array of categories - every next is next in hierarhy.
-   $cat_arr = explode(",", $wanted_data['categories']);
-   if( !empty($cat_arr) && is_array($cat_arr) ){
-	   foreach ($cat_arr as $key => $cats) {
-		   $cat_arr[$key] = explode("/", $cats);
-	   }
-   }else{
-	   $cat_arr[0] = explode("/", $wanted_data['categories']);
-   }
+    $user_id = $user->ID;
+    $type = 'existed';
+    $force_update = false;
+    $post_id = (int) get_product_by_sku_isu($wanted_data['sku']);
 
-   //remove 'root' cat
-    foreach ($cat_arr as $key => $cat) {
-		foreach ($cat as $key2 => $value) {
-			if( $value === 'root' ){
-				unset($cat_arr[$key][$key2]);
-			}
-		}
-	}
-  
-    $taxonomy = 'product_cat';
-    $parent_term = 0;//$option['child-cat-id'];
+    $wanted_data['price'] = (string) $wanted_data['price'];
+    $wanted_data['price'] = substr( $wanted_data['price'], 0, -5);
 
-    //make empty array of ids for taxonomies, we need ID of taxonomy to proper assigns it.
-    $taxes_ids = [];
-    foreach ($cat_arr as $key => $cat) {
-        $parent_term = 0;
-        $hierarhy_size = count( $cat );
-        //make sure all cats exist that product required
-        for ( $i=1; $i <= $hierarhy_size ; $i++ ) {
-            $temp_parent = null;
-            $data = null;
-            $temp_parent = term_exists( $cat[$i], $taxonomy, $parent_term );
-            if ( !$temp_parent && !is_array($temp_parent) ) {
-          		$data = wp_insert_term( $cat[$i], $taxonomy, array ( 'parent' => $parent_term ) );
-                // $data = wp_insert_term( $cat[$i], $taxonomy );
-                $taxes_ids[] = $parent_term = (int) $data['term_id'];
-                
-            }else{
-                $taxes_ids[]= $parent_term = (int) $temp_parent['term_id'];
+    if( !$post_id  && $wanted_data['name'] ){
+        $type = 'insert';
+        $post_id = wp_insert_post( [
+        'post_author'  => $user_id,
+        'post_title'   => $wanted_data['name'],
+        'post_content' => $wanted_data['description'],
+        'post_status'  => 'publish',
+        'post_type'    => 'product',
+        ]);
+    }elseif($force_update && $wanted_data['name']){
+        $type = 'force_update';
+		wp_update_post([
+			'id' => $post_id,
+			'post_title'   => $wanted_data['name'],
+			'post_content' => $wanted_data['description'],
+		]);
+    }
+    if(!$wanted_data['name'] || !(is_numeric ( (float) $wanted_data['price'] ) ) ){
+        $type = '<span style="display: block; padding: 4px; border-radius: 3px; background-color:red;">error</span>' ;
+    }
+    
+    
+    if( $type === 'insert' || $type === 'force_update' ){
+
+        //create array of categories - every next is next in hierarhy.
+        $cat_arr = explode(",", $wanted_data['categories']);
+        if( !empty($cat_arr) && is_array($cat_arr) ){
+            foreach ($cat_arr as $key => $cats) {
+                $cat_arr[$key] = explode("/", $cats);
+            }
+        }else{
+            $cat_arr[0] = explode("/", $wanted_data['categories']);
+        }
+
+        //remove 'root' cat
+            foreach ($cat_arr as $key => $cat) {
+                foreach ($cat as $key2 => $value) {
+                    if( $value === 'root' ){
+                        unset($cat_arr[$key][$key2]);
+                    }
+                }
+            }
+        
+            $taxonomy = 'product_cat';
+            $parent_term = 0;//$option['child-cat-id'];
+
+            //make empty array of ids for taxonomies, we need ID of taxonomy to proper assigns it.
+            $taxes_ids = [];
+            foreach ($cat_arr as $key => $cat) {
+                $parent_term = 0;
+                $hierarhy_size = count( $cat );
+                //make sure all cats exist that product required
+                for ( $i=1; $i <= $hierarhy_size ; $i++ ) {
+                    $temp_parent = null;
+                    $data = null;
+                    $temp_parent = term_exists( $cat[$i], $taxonomy, $parent_term );
+                    if(is_wp_error( $temp_parent )){
+                       //var_dump($temp_parent );
+                    }else{
+                        if ( !$temp_parent && !is_array($temp_parent) ) {
+                            $data = wp_insert_term( $cat[$i], $taxonomy, array ( 'parent' => $parent_term ) );
+                            // $data = wp_insert_term( $cat[$i], $taxonomy );
+                            if(is_wp_error( $data )){
+                               // var_dump($data );
+                            }else{
+                                $taxes_ids[] = $parent_term = (int) $data['term_id'];
+                            }
+                        }else{
+                            $taxes_ids[]= $parent_term = (int) $temp_parent['term_id'];
+                        }
+                    }
+                }
+            }
+        //set product taxonomies
+        wp_set_object_terms( $post_id, $taxes_ids, 'product_cat' );
+        update_post_meta( $post_id, '_visibility', 'visible' );
+        if ( $wanted_data['is_in_stock'] === '1' ) {
+                update_post_meta( $post_id, '_stock_status', 'instock');
+        }else{
+                update_post_meta( $post_id, '_stock_status', 'outofstock');
+        }
+            
+            // Adding media
+            $images[] = $wanted_data['thumbnail_image'];
+            $images[] = $wanted_data['additional_images'];
+            $images[] = $wanted_data['small_image'];
+            $images[] = $wanted_data['base_image'];
+
+            //remove diplications from array
+            $images = array_unique($images);
+            foreach ($images as $key => $image_url) {
+                if ( !UR_exists_import($image_url) ) {
+                    unset( $images[$key] );
+                }
             }
 
-        }
+            //set featured image
+            if ( isset( $images[0] ) ){
+                $image_src = media_sideload_image( $images[0], $post_id, '', 'src' );
+                $featured_img = get_attachment_id_from_src($image_src);
+                set_post_thumbnail($post_id, $featured_img);
+                unset( $images[0] );
+            }
+            //set product gallery if exist
+            $gallery_img = '';
+            if ( !empty( $images ) ){
+                foreach ($images as $key => $image_url) {
+                    $img_id = null;
+                    $image_src = media_sideload_image( $image_url, $post_id, '', 'src' );
+                    $img_id = get_attachment_id_from_src($image_src);
+                    $gallery_img .= $img_id.',';
+                    $gallery_img = rtrim($gallery_img, ',');
+                }
+                //update gallery meta
+                update_post_meta( $post_id, '_product_image_gallery' ,$gallery_img );
+            }
+
+
+
+        update_post_meta( $post_id, 'total_sales', '0' );
+        update_post_meta( $post_id, '_downloadable', 'no' );
+        update_post_meta( $post_id, '_virtual', 'no' );
+        update_post_meta( $post_id, '_regular_price', $wanted_data['price'] );
+        update_post_meta( $post_id, '_sale_price', '' );
+        update_post_meta( $post_id, '_purchase_note', '' );
+        update_post_meta( $post_id, '_featured', 'no' );
+        update_post_meta( $post_id, '_weight', '' );
+        update_post_meta( $post_id, '_length', '' );
+        update_post_meta( $post_id, '_width', '' );
+        update_post_meta( $post_id, '_height', '' );
+        update_post_meta( $post_id, '_sku', $wanted_data['sku'] );
+        update_post_meta( $post_id, '_product_attributes', array() );
+        update_post_meta( $post_id, '_sale_price_dates_from', '' );
+        update_post_meta( $post_id, '_sale_price_dates_to', '' );
+        update_post_meta( $post_id, '_price', $wanted_data['price'] );
+        update_post_meta( $post_id, '_sold_individually', '' );
+        update_post_meta( $post_id, '_manage_stock', 'no' );
+        update_post_meta( $post_id, '_backorders', 'no' );
+        update_post_meta( $post_id, '_stock', '' );
     }
-  //set product taxonomies
-   wp_set_object_terms( $post_id, $taxes_ids, 'product_cat' );
-   update_post_meta( $post_id, '_visibility', 'visible' );
-   if ( $wanted_data['is_in_stock'] === '1' ) {
-   	    update_post_meta( $post_id, '_stock_status', 'instock');
-   }else{
-   		update_post_meta( $post_id, '_stock_status', 'outofstock');
-   }
-	
-    // Adding media
-   	$images[] = $wanted_data['thumbnail_image'];
-	$images[] = $wanted_data['additional_images'];
-	$images[] = $wanted_data['small_image'];
-	$images[] = $wanted_data['base_image'];
-
-	//remove diplications from array
-	$images = array_unique($images);
-	foreach ($images as $key => $image_url) {
-		if ( !UR_exists_import($image_url) ) {
-			unset( $images[$key] );
-		}
-	}
-
-	//set featured image
-	if ( isset( $images[0] ) ){
-		$image_src = media_sideload_image( $images[0], $post_id, '', 'src' );
-		$featured_img = get_attachment_id_from_src($image_src);
-		set_post_thumbnail($post_id, $featured_img);
-		unset( $images[0] );
-	}
-	//set product gallery if exist
-	$gallery_img = '';
-	if ( !empty( $images ) ){
-		foreach ($images as $key => $image_url) {
-			$img_id = null;
-			$image_src = media_sideload_image( $image_url, $post_id, '', 'src' );
-			$img_id = get_attachment_id_from_src($image_src);
-			$gallery_img .= $img_id.',';
-			$gallery_img = rtrim($gallery_img, ',');
-		}
-		//update gallery meta
-		update_post_meta( $post_id, '_product_image_gallery' ,$gallery_img );
-	}
-
-   $wanted_data['price'] = (string) $wanted_data['price'];
-   $wanted_data['price'] = substr( $wanted_data['price'], 0, -5);
-
-   update_post_meta( $post_id, 'total_sales', '0' );
-   update_post_meta( $post_id, '_downloadable', 'no' );
-   update_post_meta( $post_id, '_virtual', 'no' );
-   update_post_meta( $post_id, '_regular_price', $wanted_data['price'] );
-   update_post_meta( $post_id, '_sale_price', '' );
-   update_post_meta( $post_id, '_purchase_note', '' );
-   update_post_meta( $post_id, '_featured', 'no' );
-   update_post_meta( $post_id, '_weight', '' );
-   update_post_meta( $post_id, '_length', '' );
-   update_post_meta( $post_id, '_width', '' );
-   update_post_meta( $post_id, '_height', '' );
-   update_post_meta( $post_id, '_sku', $wanted_data['sku'] );
-   update_post_meta( $post_id, '_product_attributes', array() );
-   update_post_meta( $post_id, '_sale_price_dates_from', '' );
-   update_post_meta( $post_id, '_sale_price_dates_to', '' );
-   update_post_meta( $post_id, '_price', $wanted_data['price'] );
-   update_post_meta( $post_id, '_sold_individually', '' );
-   update_post_meta( $post_id, '_manage_stock', 'no' );
-   update_post_meta( $post_id, '_backorders', 'no' );
-   update_post_meta( $post_id, '_stock', '' );
-   
-//    return $post_id;
+   return [
+    'id' => $post_id,
+    'type' => $type,
+    ];
 }
 
 function UR_exists_import($url){
